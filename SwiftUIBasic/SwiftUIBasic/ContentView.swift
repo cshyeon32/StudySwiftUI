73a4434ffd7fdc9bcc13985f7001a78381a8a896
cshyeon32 <139736071+cshyeon32@users.noreply.github.com>
2024-09-20 09:07:57 +0900

    create

---
+++ SwiftUIBasic/SwiftUIBasic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIBasic
+//
+//  Created by dgsw8th07 on 9/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIBasic/SwiftUIBasic/SwiftUIBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicApp.swift
+//  SwiftUIBasic
+//
+//  Created by dgsw8th07 on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
