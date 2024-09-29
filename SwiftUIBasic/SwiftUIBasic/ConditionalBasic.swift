2cb1ff5da6bfc629f41ee3a6e32c8bab4e02b13a
cshyeon32 <139736071+cshyeon32@users.noreply.github.com>
2024-09-30 03:41:04 +0900

    Conditional & Ternary
    if-else 문과 삼항연산자에 대해 공부하였다.

---
+++ SwiftUIBasic/SwiftUIBasic/ConditionalBasic.swift
@@ -0,0 +1,76 @@
+//
+//  ConditionalBasic.swift
+//  SwiftUIBasic
+//
+//  Created by dgsw8th07 on 9/30/24.
+//
+
+import SwiftUI
+
+struct ConditionalBasic: View {
+    
+    @State var showCircle : Bool = false
+    @State var showRectangel : Bool = false
+    @State var isLoding : Bool = false
+    
+    var body: some View {
+        VStack(spacing : 20){
+            //로딩 버튼
+            Button {
+                isLoding.toggle() //toggle()은 누르는거 반대로 전환 false -> true
+            } label: {
+                Text("로드중...: \(isLoding.description)") //description 불리언 값을 텍스트 타입으로 나타냄
+            }
+            //로딩 이미지
+            if isLoding {
+                ProgressView() //로딩 뷰
+            }
+            
+            //원형 버튼
+            Button{
+                showCircle.toggle()
+            } label: {
+                Text("원형 버튼: \(showCircle.description)")
+            }
+            
+            
+            //사각형 버튼
+            Button{
+                showRectangel.toggle()
+            } label: {
+                Text("사각형 버튼: \(showRectangel.description)")
+            }
+            
+            //showCircle이 true일때
+            if showCircle {
+                Circle()
+                    .frame(width: 100, height: 100)
+            }
+            
+            //showRectangel이 true일때
+            if showRectangel {
+                Rectangle()
+                    .frame(width: 100, height: 100)
+            }
+            
+            //showCircle과 showRectangel이 false일때
+            if showCircle == false && showRectangel == false { //!showCircle && !showRectangel
+                RoundedRectangle(cornerRadius: 25)
+                    .frame(width: 200, height: 100)
+            }
+            
+            //showCircle 또는 showRectangel이 true일때
+            if showCircle || showRectangel {
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(Color.pink)
+                    .frame(width: 200, height: 100)
+            }
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    ConditionalBasic()
+}
---
+++ SwiftUIBasic/SwiftUIBasic/TernaryBasic.swift
@@ -0,0 +1,60 @@
+//
+//  TernaryBasic.swift
+//  SwiftUIBasic
+//
+//  Created by dgsw8th07 on 9/30/24.
+//
+
+import SwiftUI
+//삼항연산자
+//? True일때 실행되는 logic
+//: False일때 실행되는 logic
+struct TernaryBasic: View {
+    
+    @State var isStaringState : Bool = false
+    
+    var body: some View {
+        VStack {
+            
+            //if-else 조건문
+            Button{
+                isStaringState.toggle()
+            } label: {
+                Text("if elsd 버튼 : \(isStaringState.description)")
+            }
+            
+            if isStaringState{
+                RoundedRectangle(cornerRadius: 25.0)
+                    .fill(Color.blue)
+                    .frame(width: 200, height: 100)
+            } else {
+                RoundedRectangle(cornerRadius: 25.0)
+                    .fill(Color.purple)
+                    .frame(width: 200, height: 100)
+            }
+            
+            //삼항연산자
+            Button{
+                isStaringState.toggle()
+            } label: {
+                Text("삼항연산자 버튼 : \(isStaringState.description)")
+            }
+            
+            Text(isStaringState ? "파랑" : "보라")
+//            if isStaringState {
+//                Text("파랑")
+//            } else {
+//                Text("보라")
+//            }
+            
+            RoundedRectangle(cornerRadius: isStaringState ? 25 : 0)
+                .fill(isStaringState ? Color.blue : Color.purple)
+                .frame(width: isStaringState ? 200 : 50,
+                       height: isStaringState ? 400 : 100)
+        }
+    }
+}
+
+#Preview {
+    TernaryBasic()
+}
